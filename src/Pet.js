6963d65a8808e3d342fb005e42ab2317193c7065
Brian Holt <btholt@gmail.com>
2019-01-28 21:30:11 -0800

    add Parcel build process, extract out Pet component

---
+++ src/Pet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+export default Pet;
