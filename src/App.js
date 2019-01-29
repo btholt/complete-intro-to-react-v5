4cb0048bead6d912bf68d052d00d1c90a1d15d2e
Brian Holt <btholt@gmail.com>
2019-01-28 21:30:11 -0800

    add Parcel build process, extract out Pet component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
