74a3ae9a0bd2879689406f9306f2eebce4326d54
Brian Holt <btholt@gmail.com>
2019-01-28 18:34:08 -0800

    write first React code, no JSX

---
+++ src/App.js
@@ -0,0 +1,26 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
