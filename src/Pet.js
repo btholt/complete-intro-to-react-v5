8c2bffb309a988734c99b719b6ffe7ddbafa133a
Brian Holt <btholt@gmail.com>
2019-02-17 15:21:05 -0800

    async calls to Petfinder API, update Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 const Pet = props => {
+  const { name, animal, breed, media, location, id } = props;
+
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
+
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
