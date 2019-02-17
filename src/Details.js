9d521497c36a4733754efce41d7c277728ecda21
Brian Holt <btholt@gmail.com>
2019-02-17 15:21:05 -0800

    async calls to Petfinder API, update Pet component

---
+++ src/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = () => {
+  return <h1>hi!</h1>;
+};
+
+export default Details;
---
+++ src/Results.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map(pet => {
+          return (
+            <Pet
+              animal={pet.type}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breeds.primary}
+              media={pet.photos}
+              location={`${pet.contact.address.city}, ${
+                pet.contact.address.state
+              }`}
+              id={pet.id}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Results;
