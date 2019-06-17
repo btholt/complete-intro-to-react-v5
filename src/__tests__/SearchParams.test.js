21bfc3ba0f1674e82b6aa0784781e43b6a3ff609
Daniel Afonso <daniel.afonso@farfetch.com>
2019-06-17 12:07:53 +0100

    fix/react-testing-library errors after installing

--- src/__tests__/SearchParams.test.js
+++ src/__tests__/SearchParams.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, cleanup } from "react-testing-library";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import pet, { _breeds, _dogs, ANIMALS } from "@frontendmasters/pet";
 import SearchParams from "../SearchParams";
 
