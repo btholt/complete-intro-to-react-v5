4847b6d6cbf01354d52aea3ffa907410f2a667a3
Richard Li <richardli125@gmail.com>
2019-11-09 19:29:03 -0500

    added details for pets

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,46 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = () => {
-  return <h1>hi!</h1>;
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${
+          animal.contact.address.state
+        }`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  render() {
+    const { animal, breed, location, description, name } = this.state;
+
+    if (this.state.loading) return <h1>Loading...</h1>
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location} `}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Details;
