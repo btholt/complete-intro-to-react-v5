ae11794e42fd6cbd28e71b84cd112d05184be9ae
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-11-25 10:00:48 -0800

    passing in component props - commit 3

--- src/AppPractice.js
+++ src/AppPractice.js
@@ -1,21 +1,30 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Mimi"),
-        React.createElement("h2", {}, "Cat"),
-        React.createElement("h2", {}, "Ginger Tabby"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
     ]);
 };
 
 
 const App = () => {
-  return React.createElement(
-    "div",
-    { id: "something-important" },
-    [
+  return React.createElement( "div", { id: "something-important" }, [
         React.createElement("h1", {}, "Adopt me!"),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet)
+        React.createElement(Pet, { 
+            name: "Mimi", 
+            animal: "Cat", 
+            breed: "Ginger Tabby"
+        }),
+        React.createElement(Pet, { 
+            name: "Kylo Ren", 
+            animal: "Cat", 
+            breed: "Blue Grey Mix"
+        }),
+        React.createElement(Pet, { 
+            name: "Teddy", 
+            animal: "Dog", 
+            breed: "Husky"
+        })
     ]);
 };
 
