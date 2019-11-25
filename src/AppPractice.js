8aee5b20f916dcdb71a9938b23a5a967420346ed
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-11-25 10:02:04 -0800

    commit 4: deconstructing props

--- src/AppPractice.js
+++ src/AppPractice.js
@@ -1,8 +1,8 @@
-const Pet = (props) => {
+const Pet = ({ name, animal, breed }) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed)
     ]);
 };
 
