9c777aa77fb687add847632c708e092171dc2f1a
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-11-25 11:02:25 -0800

    commit 6

--- src/Practice/SearchParamsPractice.js
+++ src/Practice/SearchParamsPractice.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParamsPrac = () => {
-    const location = "Seattle, WA";
+    // const location = "Seattle, WA";
+    // default state of Seattle
+    // setLocation = updates the state (re-rendering)
+    // getting your hook - array of current state and the update state function
+    // no hooks inside if statements or for loops
+    const [location, setLocation] = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("dog");
 
     return (
         <div className="search-params-prac">
@@ -12,7 +19,22 @@ const SearchParamsPrac = () => {
                         id="location" 
                         value={location} 
                         placeholder="Location" 
+                        onChange={e => setLocation(e.target.value)}
                     />
+                </label>
+                <label htmlFor="animal">
+                    animal
+                    <select 
+                        id="animal" 
+                        value={animal} 
+                        placeholder="Animal" 
+                        onChange={e => setAnimal(e.target.value)} 
+                        onBlur={e => setAnimal(e.target.value)} >
+                        <option>All</option>
+                        {ANIMALS.map((animal) => (
+                            <option key={animal} value={animal}>{animal}</option>
+                        ))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
