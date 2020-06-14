e2eec1217ee9dfa5c66b5216c6acdad68165fb91
kumarodian <sachinyadav607@gmail.com>
2020-06-14 13:19:05 +0530

    test mode

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <ThemeContext.Provider value={theme}>
       <div>
         <header>
-          <Link to="/">Adopt Me!</Link>
+          <Link to="/">Adopt Me! Very Soon</Link>
         </header>
         <Router>
           <SearchParams path="/" />
