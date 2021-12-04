0109ff9192a6255dc385d2f90d1a5c5f999f3f05
Shane Jeremich <shane.jeremich@gmail.com>
2021-12-03 22:46:18 -0700

    updated paths

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ const App = () => {
     <ThemeContext.Provider value={theme}>
       <div>
         <header>
-          <Link to="/">Adopt Me!</Link>
+          <Link to="/Complete-Intro-To-React-v5/">Adopt Me!</Link>
         </header>
         <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
+          <SearchParams path="/Complete-Intro-To-React-v5/" />
+          <Details path="/Complete-Intro-To-React-v5/details/:id" />
         </Router>
       </div>
     </ThemeContext.Provider>
